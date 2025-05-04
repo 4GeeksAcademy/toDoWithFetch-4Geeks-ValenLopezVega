c40c8bdca00762a4e4695e9aa9e9163e6a246b88
Valentina López Vega <37059478+ValenLopezVega@users.noreply.github.com>
2025-05-04 23:51:37 +0000

    made the functional to do list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+	const [task, setTask] = useState('');
+	const [tasks, setTasks] = useState([]);
+
+	function addTask() {
+		if (task.trim === '') return;
+		setTasks([...tasks, task]);
+		setTask('');
+	}
+
+	function deleteTask(indexToDelete){
+		const updatedTasks = tasks.filter((_, index) => index != indexToDelete);
+		setTasks(updatedTasks);
+	}
+
+	return (
+		<div className="container">
+			<div className="row d-flex justify-content-center">
+				<div className="col-8 col-md-6 flex-column">
+					<h1 className="text-center py-3">todos</h1>
+					<div className="bg-light border shadow">
+						<form onSubmit={(e) => { e.preventDefault(); addTask(); }}>
+							<input
+								className="form-control border-0"
+								type="text"
+								placeholder="What needs to be done?"
+								value={task}
+								onChange={(e) => {setTask(e.target.value)}}
+							/>
+						</form>
+						<ul className="list-group">
+							{
+								tasks.map((item, index) => (
+									<li key={index} className="list-group-item d-flex justify-content-between align-items-center">
+										{item}
+										<span onClick={() => deleteTask(index)}>
+											<i className="fas fa-times"></i>
+										</span>
+									</li>
+								))
+							}
+						</ul>
+						<p className="text-body-tertiary px-3 py-2 m-0 border-top bg-light" >{`${tasks.length} tasks left`}</p>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
